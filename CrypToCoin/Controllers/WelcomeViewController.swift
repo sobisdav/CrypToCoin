ae5595ebf918c220a9300589d463be22dc3460b9
David Sobíšek <sobisdav@fit.cvut.cz>
2022-04-10 00:22:02 +0200

    Fixed CalculatorButton and added possible calculator screen design. Refactored folder to prepare it for MVC.

---
+++ CrypToCoin/Controllers/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CrypToCoin
+//
+//  Created by David Sobíšek on 07.04.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var calculatorButton: UIButton!
+    @IBAction func calculatorButtonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        calculatorButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        calculatorButton.titleLabel?.minimumScaleFactor = 0.5
+        calculatorButton.layer.cornerRadius = 10.0
+    }
+
+
+}
+
