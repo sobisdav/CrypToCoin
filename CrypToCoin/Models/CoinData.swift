b6455d588cd2dbe4d65697c659677f8320bcb3ba
David Sobíšek <sobisdav@fit.cvut.cz>
2022-04-12 12:17:36 +0200

    Created model classes for coin and crypto data.

---
+++ CrypToCoin/Models/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  CrypToCoin
+//
+//  Created by David Sobíšek on 11.04.2022.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
---
+++ CrypToCoin/Models/CryptoData.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoData.swift
+//  CrypToCoin
+//
+//  Created by David Sobíšek on 11.04.2022.
+//
+
+import Foundation
+
+class CryptoData {
+    var name: String?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
