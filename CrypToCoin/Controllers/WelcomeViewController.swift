53bf5805eb16e00c6e769da7e066fbfa8c818935
David Sobíšek <sobisdav@fit.cvut.cz>
2022-05-10 23:38:03 +0200

    Added Typewriter like animation package to title.

--- CrypToCoin/Controllers/WelcomeViewController.swift
+++ CrypToCoin/Controllers/WelcomeViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import GhostTypewriter
 
 class WelcomeViewController: UIViewController {
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: TypewriterLabel!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.text = "CrypToCoin 🪙"
+        titleLabel.startTypewritingAnimation()
+        titleLabel.typingTimeInterval = 0.05
         loginButton.titleLabel?.adjustsFontSizeToFitWidth = true
         loginButton.titleLabel?.minimumScaleFactor = 0.5
         loginButton.layer.cornerRadius = 10.0
